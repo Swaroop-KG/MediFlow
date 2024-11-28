44c6ca9c3cc99b41a37df3ad22f6bbe3648ceffc
swaroop-12 <swaroop.k.g49@gmail.com>
2024-11-28 21:32:19 +0530

    last commit for deploy

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-import app from "./app.js";
-import cloudinary from "cloudinary";
 import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { errorMiddleware } from "./middlewares/error.js";
+import messageRouter from "./router/messageRouter.js";
+import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js";
+import cloudinary from "cloudinary";
+
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -9,12 +18,56 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
+const app = express();
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server listening at port ${port}`);
+});
+ 
 
+config({ path: "./config/config.env" });
+
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+});
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+app.use("/api/v1/message", messageRouter);
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
+
+dbConnection();
+
+
+
+
+app.use(errorMiddleware);
 
 app.use(express.static(path.join(__dirname, '/frontend/dist')));
 app.get('*', (req, res) => {
@@ -27,7 +80,3 @@ app.get('*', (req, res) => {
 });
 
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`Server listening at port ${port}`);
-});
